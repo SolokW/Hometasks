359a4d531cfa3e58df672bf031504b7c8e318ad7
Alexey <a@a.arbuz>
2022-08-16 07:43:24 +0300

    New save

--- Hometask1_Sem1/Example1_Compare/Program.cs
+++ Hometask1_Sem1/Example1_Compare/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿//  Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
@@ -14,6 +14,7 @@ Console.Write("Большее число: ");
 Console.WriteLine(num1);
 Console.Write("Меньшее число: ");
 Console.WriteLine(num2);
+Console.WriteLine($"a = {num1}, b = {num2} -> {num1}");
 }
 else
 {
@@ -21,4 +22,5 @@ else
     Console.WriteLine(num2);
     Console.Write("Меньшее число: ");
     Console.WriteLine(num1);
+    Console.WriteLine($"a = {num1}; b = {num2} -> {num2}");
 }

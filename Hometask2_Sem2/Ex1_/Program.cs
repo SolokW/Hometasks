44f14f87d8e62c769903888338c1e15c717a253b
Alexey <a@a.arbuz>
2022-08-17 09:07:46 +0300

    Hometask 2 Sem 2

---
+++ Hometask2_Sem2/Ex1_/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//(1)
+// int a = 456;
+// int b = 782;
+// int c = 918;
+
+// int valA = a / 10;
+// int valB = b / 10;
+// int valC = c / 10;
+
+// int valAA = valA % 10;
+// int valBB = valB % 10;
+// int valCC = valC % 10;
+
+
+// Console.WriteLine($"{a} -> {valAA}");
+// Console.WriteLine($"{b} -> {valBB}");
+// Console.WriteLine($"{c} -> {valCC}");
+
+//(2)
+int HOP(int value)
+{
+    int val = value / 10;
+    int end = val % 10;
+    return end;
+}
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{num} - > ");
+System.Console.WriteLine(HOP(num));
+
---
+++ Hometask2_Sem2/Ex2_/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+// int HOP(int value)
+// {
+//     int a = value / 100;
+//     int b = a % 10;
+//     return b;
+// }
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (HOP(num) == 0)
+// {
+//     Console.Write($"{num} -> Третьей цифры нет");
+// }
+// else
+// {
+//     Console.Write($"{num} - > ");
+// System.Console.WriteLine(HOP(num));
+// }
+
+// Не могу понять, почему оно работает, но оно работает!
+// 30 минут уже понять не могу, почему программа при вводе "123456" не выводит "124". Я же только от "3" избавился.
+
+int HEY(int value)
+{
+    int a = value / 100;
+    int b = a % 10;
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        b = b % 10;
+    }
+    return b;
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (HEY(num) == 0)
+{
+    Console.Write($"{num} -> Третьей цифры нет");
+}
+else
+{
+    Console.Write($"{num} - > ");
+System.Console.WriteLine(HEY(num));
+}

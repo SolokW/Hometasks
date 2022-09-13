a55085ba0df677355879e2578a3070346c1b03d3
Alexey <a@a.arbuz>
2022-09-13 21:27:08 +0300

    Hometask 4 done

---
+++ Hometask3_Sem3/Ex1_Palindrom/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите метод, который принимает на вход шестизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// строки использовать нельзя!
+
+
+Console.WriteLine("Введите шестизначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+// 123456
+
+char[] hop = num.ToString().ToCharArray();
+// Console.Write(hop);
+int x = hop.Length - 1;
+
+ for (int i = 0; i < hop.Length/2; i++)
+        {
+            // Console.Write(hop[x - i]);
+            // Console.Write(hop[i]);
+
+            if (hop[i] != hop[x-i])
+            {
+                Console.Write($"{num} -> нет");
+                Environment.Exit(0);
+            }
+        }
+Console.Write($"{num} -> да");
+  
+
+
+// int a = num;
+// int b = num;
+// int[] o = {};
+// while (b>0)
+// { 
+//     a = b % 10;
+//     b = b / 10;
+//     Console.Write(a);
+
+// }
+
+// int mun = Convert.ToInt32(Console.ReadLine());
+// int g = num - mun;
+// Console.Write(g);
+
+
+// if (mun == num) Console.WriteLine($"{num} -> да");
+// else Console.WriteLine($"{num} -> нет");
---
+++ Hometask3_Sem3/Ex2_/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 21
+
+// Напишите метод, который принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координаты x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты x2: ");
+
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты x2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты x2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между точками в  3D пространстве равно {dist}");
+
+
+
